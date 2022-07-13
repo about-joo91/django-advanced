34f4bce20561c0c7cde35b737170f9148763ae28
about-joo91 <about.joo91@gmail.com>
2022-07-13 19:50:13 +0900

    test_case 추가

--- sparta/settings.py
+++ sparta/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from typing import List
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-#)8zihi*t%owyxt&qn(pu#-gkv1kvcnw%ngyzue+o-$#fu9x=9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
--- sparta/urls.py
+++ sparta/urls.py
@@ -1,12 +1,16 @@
+from typing import Dict
+
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI
+
 
 api = NinjaAPI()
 
 
 @api.get("/add")
-def add(request, a: int, b: int):
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
     return {"result": a + b}
 
 
