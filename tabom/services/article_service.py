a2923e8a77ac694b17ea7e29898706615c8fe599
about-joo91 <about.joo91@gmail.com>
2022-07-17 20:51:33 +0900

    article 생성, 삭제 구현

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -4,8 +4,14 @@ from tabom.models import Article
 from tabom.models.like import Like
 
 
-def get_an_article(article_id: int) -> Article:
-    return Article.objects.filter(id=article_id).get()
+def create_an_article(title: str) -> Article:
+    return Article.objects.create(title=title)
+
+
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
 
 
 def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
@@ -16,3 +22,7 @@ def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]
             offset : offset + limit
         ]
     )
+
+
+def delete_an_article(article_id: int) -> None:
+    Article.objects.filter(id=article_id).delete()
