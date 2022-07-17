117f4f6355608a8be5531344389ae2b4f142d8ef
about-joo91 <about.joo91@gmail.com>
2022-07-17 19:29:23 +0900

    prefetch_related 써보기

--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -14,7 +14,6 @@ class TestArticleService(TestCase):
 
         # When
         result_article = get_an_article(article.id)
-
         # Then
         self.assertEqual(article.id, result_article.id)
         self.assertEqual(title, result_article.title)
@@ -34,9 +33,26 @@ class TestArticleService(TestCase):
         do_like(user.id, articles[-1].id)
 
         # When
-        result_articles = get_article_list(0, 10)
+        # with CaptureQueriesContext(connection) as ctx:
+        with self.assertNumQueries(3):
+            result_articles = get_article_list(user.id, 0, 10)
+            result_counts = [a.like_set.count() for a in result_articles]
+
+            # Then
+            self.assertEqual(len(result_articles), 10)
+            self.assertEqual(1, result_counts[0])
+            self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
+
+    def test_get_article_list_should_contain_my_like_when_like_exists(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        article1 = Article.objects.create(title="artice1")
+        like = do_like(user.id, article1.id)
+        Article.objects.create(title="article2")
+
+        # When
+        articles = get_article_list(user.id, 0, 10)
 
         # Then
-        self.assertEqual(len(result_articles), 10)
-        self.assertEqual(1, result_articles[0].like_set.count())
-        self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
+        self.assertEqual(like.id, articles[1].my_likes[0].id)
+        self.assertEqual(0, len(articles[0].my_likes))
